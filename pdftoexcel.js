094c15bffb407e0549a8a8a3478211f9872d99cf
fred <freddyantoniomachado@gmail.com>
2024-05-04 12:22:50 -0400

    create folder function

--- pdftoexcel.js
+++ pdftoexcel.js
@@ -1,10 +1,21 @@
 const pdf2excel = require('pdf-to-excel');
+const fs = require('node:fs');
 
+//get month
+const d = new Date();
+let month = d.getMonth();
+const months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+const folderName = '../resources/'+months[month];
+
+//convert xls to pdf
 try { 
-    
-    pdf2excel.genXlsx('../resources/cestaticket.pdf', '../resources/cestaticket.xlsx'); 
-    pdf2excel.genXlsx('../resources/ticketplus.pdf', '../resources/ticketplus.xlsx'); 
-    pdf2excel.genXlsx('../resources/panamcred.pdf', '../resources/panamcred.xlsx'); 
+    //if folder does not exist
+    if (!fs.existsSync(folderName)) {
+      fs.mkdirSync(folderName);
+    }
+    pdf2excel.genXlsx('../resources/cestaticket.pdf', folderName+'/cestaticket.xlsx'); 
+    pdf2excel.genXlsx('../resources/ticketplus.pdf', folderName+'/ticketplus.xlsx'); 
+    pdf2excel.genXlsx('../resources/panamcred.pdf', folderName+'/panamcred.xlsx'); 
 } catch (err) { 
     console.error(err); 
 }
